73c8b5d1fe0e4f7d2c9150ed3b9018706c6d1d66
Jakub Prüher <jacobnzw@gmail.com>
2018-06-03 17:23:43 +0200

    PCA demo added, BGR space appears to require the smallest number of components; raw pixel features added.

--- main.py
+++ main.py
@@ -82,8 +82,85 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, featu
     else:
         return result
 
-# TODO: more features: color histograms, spatial binning of color (raw pixels of subsampled image)
-# TODO: experiment with PCA dimensionality reduction of the raw pixel values
+
+def get_raw_pixel_features(img, color_space='BGR', size=(32, 32)):
+    """
+    Feature vector of raw pixel values in given color space and resizing.
+
+    Parameters
+    ----------
+    img :
+        BGR image
+    color_space : str {'BGR', 'HSV', 'HLS', 'LUV', 'YUV', 'YCrCb'}
+        Which color space convert the image to
+    size : (int, int)
+        Size of the resized image
+
+    Returns
+    -------
+
+    """
+
+    # Convert image to new color space (if specified)
+    if color_space != 'BGR':
+        if color_space == 'HSV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_BGR2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    else:
+        feature_image = np.copy(img)
+
+    features = cv2.resize(feature_image, size).ravel()
+    return features
+
+
+# TODO: more features: color histograms
+def pca_demo(car_filenames, noncar_filenames, var_cutoff=0.95):
+    """
+    Attempt dimensionality reduction of raw pixel data.
+
+    Parameters
+    ----------
+    img
+    var_cutoff
+
+    Returns
+    -------
+
+    """
+    features = []
+    labels = []
+    for file in car_filenames:
+        img = cv2.imread(file)
+        feat = get_raw_pixel_features(img, color_space='BGR')
+        features.append(feat)
+        labels.append(LABEL_CAR)
+    for file in noncar_filenames:
+        img = cv2.imread(file)
+        feat = get_raw_pixel_features(img, color_space='BGR')
+        features.append(feat)
+        labels.append(LABEL_NONCAR)
+
+    # shuffle features and labels
+    features, labels = shuffle(np.array(features), np.array(labels))
+    features, labels = features[:len(features)//2, :], labels[:len(labels)//2]
+
+    from sklearn.decomposition import PCA
+    dimred = PCA()
+    dimred.fit(features)
+    # print(np.cumsum(dimred.explained_variance_ratio_[:100]))
+    num_comp = np.argwhere(np.cumsum(dimred.explained_variance_ratio_) > var_cutoff).min()
+    print('{:d} components needed to preserve {:.2f}% of variance'.format(num_comp, var_cutoff*100))
+    # plt.plot(dimred.explained_variance_)
+    plt.plot(dimred.explained_variance_ratio_[:100])
+    plt.show()
+
 
 
 def extract_features(car_filenames, noncar_filenames):
@@ -113,29 +190,31 @@ def extract_features(car_filenames, noncar_filenames):
 
 
 if __name__ == '__main__':
-    # car_list, noncar_list = prep_data_files()
+    car_list, noncar_list = prep_data_files()
     # X, y = extract_features(car_list, noncar_list)
 
     # # save extracted features and labels
     # np.savez('data_2k', X, y, features=X, labels=y)
 
-    # load features and labels
-    data = np.load('data_2k_hog.npz')
-    X, y = data['features'], data['labels']
+    # # load features and labels
+    # data = np.load('data_2k_hog.npz')
+    # X, y = data['features'], data['labels']
+    #
+    # from sklearn.model_selection import train_test_split, GridSearchCV
+    # from sklearn.svm import LinearSVC, SVC
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
+    #
+    # print('Fitting LinearSVC...')
+    # clf = LinearSVC()
+    # clf.fit(X_train, y_train)
+    # print('Mean accuracy {}'.format(clf.score(X_test, y_test)))
+    #
+    # # TODO: finish preliminary version of the whole pipeline
+    #
+    # print('Fitting LinearSVC grid search...')
+    # from sklearn.metrics import accuracy_score, make_scorer
+    # clf = GridSearchCV(clf, {'C': np.logspace(-3, 3, 10)}, scoring=make_scorer(accuracy_score), n_jobs=3)
+    # clf.fit(X_train, y_train)
+    # print('Mean accuracy {:.2f} (Best params: {:.4e})'.format(clf.score(X_test, y_test), clf.best_params_['C']))
 
-    from sklearn.model_selection import train_test_split, GridSearchCV
-    from sklearn.svm import LinearSVC, SVC
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
-
-    print('Fitting LinearSVC...')
-    clf = LinearSVC()
-    clf.fit(X_train, y_train)
-    print('Mean accuracy {}'.format(clf.score(X_test, y_test)))
-
-    # TODO: finish preliminary version of the whole pipeline
-
-    print('Fitting LinearSVC grid search...')
-    from sklearn.metrics import accuracy_score, make_scorer
-    clf = GridSearchCV(clf, {'C': np.logspace(-3, 3, 10)}, scoring=make_scorer(accuracy_score), n_jobs=3)
-    clf.fit(X_train, y_train)
-    print('Mean accuracy {:.2f} (Best params: {:.4e})'.format(clf.score(X_test, y_test), clf.best_params_['C']))
+    pca_demo(car_list, noncar_list)
