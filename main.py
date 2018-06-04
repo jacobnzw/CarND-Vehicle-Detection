781d0326033f7ddf4debb071852f39f367ce5b3a
Jakub Prüher <jacobnzw@gmail.com>
2018-06-04 21:18:50 +0200

    Sliding windows and draw boxes code copied from Udacity. Demo created.
    LinearSVC predicts with quite a lot of false positives. Considering extending with additional features and increasing size of the dataset.

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class VehicleDetector:
 
 LABEL_CAR = 1
 LABEL_NONCAR = 0
+IMGDIR_TEST = 'test_images'
 
 
 def prep_data_files():
@@ -151,16 +152,19 @@ def pca_demo(car_filenames, noncar_filenames, var_cutoff=0.95):
     features, labels = shuffle(np.array(features), np.array(labels))
     features, labels = features[:len(features)//2, :], labels[:len(labels)//2]
 
+    # feature normalization
+    features = StandardScaler().fit_transform(features)
+
     from sklearn.decomposition import PCA
-    dimred = PCA()
+    dimred = PCA(n_components=features.shape[0])
     dimred.fit(features)
     # print(np.cumsum(dimred.explained_variance_ratio_[:100]))
     num_comp = np.argwhere(np.cumsum(dimred.explained_variance_ratio_) > var_cutoff).min()
     print('{:d} components needed to preserve {:.2f}% of variance'.format(num_comp, var_cutoff*100))
-    # plt.plot(dimred.explained_variance_)
-    plt.plot(dimred.explained_variance_ratio_[:100])
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(dimred.explained_variance_ratio_[:100])
+    ax[1].plot(np.cumsum(dimred.explained_variance_ratio_))
     plt.show()
-
 
 
 def extract_features(car_filenames, noncar_filenames):
@@ -189,32 +193,130 @@ def extract_features(car_filenames, noncar_filenames):
     return features, labels
 
 
+def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
+                 xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
+    """
+    Records corners of a window in each step as it slides across the image (from top left to bottom right).
+
+    Parameters
+    ----------
+    img : ndarray
+    x_start_stop : [int, int] or [None, None]
+        Starting and stopping position of a window sliding in x (horizontal) direction.
+    y_start_stop : [int, int] or [None, None]
+        Starting and stopping position of a window sliding in y (vertical) direction.
+    xy_window : (int, int)
+        Window width and height
+    xy_overlap : (int, int)
+        Window overlap in x (horizontal) and y (vertical) directions.
+
+    Notes
+    -----
+    Taken from Udacity's sliding window implementation. Minor modifications added.
+
+    Returns
+    -------
+        List of tuples ((startx, starty), (endx, endy)), where (startx, starty) is top left corner and (endx, endy) is
+        bottom right window corner.
+    """
+    # If x and/or y start/stop positions not defined, set to image size
+    if x_start_stop[0] is None:
+        x_start_stop[0] = 0
+    if x_start_stop[1] is None:
+        x_start_stop[1] = img.shape[1]
+    if y_start_stop[0] is None:
+        y_start_stop[0] = 0
+    if y_start_stop[1] is None:
+        y_start_stop[1] = img.shape[0]
+    # Compute the span of the region to be searched
+    xspan = x_start_stop[1] - x_start_stop[0]
+    yspan = y_start_stop[1] - y_start_stop[0]
+    # Compute the number of pixels per step in x/y
+    nx_pix_per_step = np.int(xy_window[0]*(1 - xy_overlap[0]))
+    ny_pix_per_step = np.int(xy_window[1]*(1 - xy_overlap[1]))
+    # Compute the number of windows in x/y
+    nx_buffer = np.int(xy_window[0]*(xy_overlap[0]))
+    ny_buffer = np.int(xy_window[1]*(xy_overlap[1]))
+    nx_windows = np.int((xspan-nx_buffer)/nx_pix_per_step)
+    ny_windows = np.int((yspan-ny_buffer)/ny_pix_per_step)
+    # Initialize a list to append window positions to
+    window_list = []
+    # Loop through finding x and y window positions
+    # Note: you could vectorize this step, but in practice
+    # you'll be considering windows one by one with your
+    # classifier, so looping makes sense
+    for ys in range(ny_windows):
+        for xs in range(nx_windows):
+            # Calculate window position
+            startx = xs*nx_pix_per_step + x_start_stop[0]
+            endx = startx + xy_window[0]
+            starty = ys*ny_pix_per_step + y_start_stop[0]
+            endy = starty + xy_window[1]
+            # Append window position to list
+            window_list.append(((startx, starty), (endx, endy)))
+    # Return the list of windows
+    return window_list
+
+
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    # Make a copy of the image
+    imcopy = np.copy(img)
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
+    # Return the image copy with boxes drawn
+    return imcopy
+
+
 if __name__ == '__main__':
+    # TODO: finish preliminary version of the whole pipeline
     car_list, noncar_list = prep_data_files()
     # X, y = extract_features(car_list, noncar_list)
 
     # # save extracted features and labels
     # np.savez('data_2k', X, y, features=X, labels=y)
 
-    # # load features and labels
-    # data = np.load('data_2k_hog.npz')
-    # X, y = data['features'], data['labels']
+    # load features and labels
+    data = np.load('data_2k_hog.npz')
+    X, y = data['features'], data['labels']
     #
-    # from sklearn.model_selection import train_test_split, GridSearchCV
-    # from sklearn.svm import LinearSVC, SVC
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
-    #
-    # print('Fitting LinearSVC...')
-    # clf = LinearSVC()
-    # clf.fit(X_train, y_train)
-    # print('Mean accuracy {}'.format(clf.score(X_test, y_test)))
-    #
-    # # TODO: finish preliminary version of the whole pipeline
-    #
+    from sklearn.model_selection import train_test_split, GridSearchCV
+    from sklearn.svm import LinearSVC, SVC
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
+
+    print('Fitting LinearSVC...')
+    clf = LinearSVC()
+    clf.set_params(C=0.004)
+    clf.fit(X_train, y_train)
+    print('Mean accuracy {}'.format(clf.score(X_test, y_test)))
+
     # print('Fitting LinearSVC grid search...')
     # from sklearn.metrics import accuracy_score, make_scorer
     # clf = GridSearchCV(clf, {'C': np.logspace(-3, 3, 10)}, scoring=make_scorer(accuracy_score), n_jobs=3)
     # clf.fit(X_train, y_train)
     # print('Mean accuracy {:.2f} (Best params: {:.4e})'.format(clf.score(X_test, y_test), clf.best_params_['C']))
+
+    print('Processing test image...')
+    test_files = glob.glob(os.path.join(IMGDIR_TEST, '*.jpg'))
+    img = cv2.cvtColor(cv2.imread(test_files[2]), cv2.COLOR_BGR2GRAY)
+    windows = slide_window(img, y_start_stop=[400, 500], x_start_stop=[600, None], xy_overlap=(0.5, 0.5))
+    test_features = []
+    for win in windows:
+        crop = img[win[0][1]:win[1][1], win[0][0]:win[1][0]]
+        test_features.append(get_hog_features(crop, 9, 8, 2))
+    test_features = np.array(test_features)
+    # feature scaling
+    scaler = StandardScaler()
+    test_features_scaled = scaler.fit_transform(test_features)
+
+    y_pred = clf.predict(test_features_scaled)
+    # which windows were predicted as cars
+    car_win = [windows[i] for i in np.argwhere(y_pred == LABEL_CAR).squeeze()]
+    img_boxes = draw_boxes(img, car_win, thick=3)
+
+    fig, ax = plt.subplots(1, 1)
+    ax.imshow(img_boxes, cmap='gray')
+    plt.show()
 
     pca_demo(car_list, noncar_list)
